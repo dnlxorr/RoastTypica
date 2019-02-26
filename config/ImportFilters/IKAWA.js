0b5e382fcd5a97f59db927b3a59abd4a382c4b70
Neal Wilson <neal@typica.us>
2019-02-26 15:38:04 -0600

    Support IKAWA CSV with headers: time, state, temp set, exaust temp, fan set (%), fan speed (RPM) heater power (%)

--- config/ImportFilters/IKAWA.js
+++ config/ImportFilters/IKAWA.js
@@ -11,17 +11,41 @@ pluginContext.table.addOutputControlColumn(3);
 pluginContext.table.addOutputControlColumn(4);
 pluginContext.table.addOutputAnnotationColumn(5);
 var lines = pluginContext.data.split('\n');
-for(var i = 0; i < lines.length; i++) {
-	var fields = lines[i].split(',');
-	if(fields[5] == "roasting") {
-		var time = new QTime;
-		time = time.addSecs(Number(fields[0]));
-		pluginContext.newMeasurement(new Measurement(Units.convertTemperature(fields[4], Units.Celsius, Units.Fahrenheit), time), 1);
-		pluginContext.newMeasurement(new Measurement(Units.convertTemperature(fields[2], Units.Celsius, Units.Fahrenheit), time), 2);
-		pluginContext.newMeasurement(new Measurement(fields[1], time, Units.Unitless), 3);
-		pluginContext.newMeasurement(new Measurement(fields[6], time, Units.Unitless), 4);
-	}
+var headers = lines[0].split(',');
+if(headers[1] == 'state') {
+    importV2(lines);
+} else {
+    importV1(lines);
 }
 for(var i = 1; i < 5; i++) {
 	pluginContext.table.newAnnotation("End", i, 5);
 }
+
+function importV1(lines) {
+    for(var i = 0; i < lines.length; i++) {
+        var fields = lines[i].split(',');
+        if(fields[5] == "roasting") {
+            var time = new QTime;
+            time = time.addSecs(Number(fields[0]));
+            pluginContext.newMeasurement(new Measurement(Units.convertTemperature(fields[4], Units.Celsius, Units.Fahrenheit), time), 1);
+            pluginContext.newMeasurement(new Measurement(Units.convertTemperature(fields[2], Units.Celsius, Units.Fahrenheit), time), 2);
+            pluginContext.newMeasurement(new Measurement(fields[1], time, Units.Unitless), 3);
+            pluginContext.newMeasurement(new Measurement(fields[6], time, Units.Unitless), 4);
+        }
+    }
+}
+
+function importV2(lines) {
+    for(var i = 0; i < lines.length; i++) {
+        var fields = lines[i].split(',');
+        if(fields[1] == "roasting") {
+            var time = new QTime;
+            time = time.addSecs(Number(fields[0]));
+            pluginContext.newMeasurement(new Measurement(Units.convertTemperature(fields[3], Units.Celsius, Units.Fahrenheit), time), 1);
+            pluginContext.newMeasurement(new Measurement(Units.convertTemperature(fields[2], Units.Celsius, Units.Fahrenheit), time), 2);
+            pluginContext.newMeasurement(new Measurement(fields[4], time, Units.Unitless), 3);
+            pluginContext.newMeasurement(new Measurement(fields[6], time, Units.Unitless), 4);
+        }
+    }
+}
+
